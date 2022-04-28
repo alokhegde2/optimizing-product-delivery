da48e842331b54efc6929029b544b9c445aaa9c6
Alok Hegde <Alok.Hegde@novigo.com>
2022-04-28 12:55:20 +0530

    Added fill form and header section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
 import WelcomeScreen from './screens/welcome/WelcomeScreen';
 import FileUploadScreen from './screens/file_upload/FileUploadScreen';
+import FormScreen from './screens/form/FormScreen';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<WelcomeScreen />} />
         <Route path='/file-upload' element={<FileUploadScreen />} />
+        <Route path='/form' element={<FormScreen />} />
       </Routes>
     </div>
   );
