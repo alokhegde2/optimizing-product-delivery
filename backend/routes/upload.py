e70084c76bd4134371c6bdefa94625d2382f10b6
Alok Hegde <hegdealok@yahoo.com>
2022-06-08 14:15:49 +0530

    Connected our backend to frontend

--- backend/routes/upload.py
+++ backend/routes/upload.py
@@ -1,72 +1,76 @@
 from flask import Flask, request, jsonify, Blueprint
 import requests
+from bson.objectid import ObjectId
 
 from database import mongoClient
 
 upload = Blueprint('upload', __name__)
 
-#Uploading file url
+# Uploading file url
+# This route to upload file which is majorly in xlsx or csv format
+# This is not yet done
+# I'm planing to keep this for future scope
+
 
 @upload.route('/upload_file', methods=["GET"])
 async def upload_file():
     fileUrl = request.json["fileUrl"]
 
     opdData = {
-    "datatype":"file",
-    "data":[],
-    "fileUrl":fileUrl,
+        "datatype": "file",
+        "data": [],
+        "fileUrl": fileUrl,
     }
 
     try:
-        dataId = mongoClient.db.OPD_Data.insert_one(opdData);
+        dataId = mongoClient.db.OPD_Data.insert_one(opdData)
     except Exception as e:
         return jsonify(
             message="Error Occured",
             error=e,
-            ),400
-
+        ), 400
 
     return jsonify(
         message="Data Added Successfully"
-        ),200
+    ), 200
 
 
 # Uploading data through form
+# This route to add data to the db
+# It recives data from the form which is implemented in React.js
+# And it stores those data into the Mongodb atlas
 
-@upload.route('/upload_form',methods=["POST"])
+@upload.route('/upload_form', methods=["POST"])
 async def upload_form():
+    # Getting address from the request body
+    
     address = request.json["address"]
 
+    # Here opdData is the data format which will be got saved in the database
+
     opdData = {
-    "datatype":"form",
-    "data":address
+        "datatype": "form",
+        "data": address
     }
 
     try:
-        dataId = mongoClient.db.OPD_Data.insert_one(opdData);
+        # Here we are  running querry to insert data to the databse
+
+        dataId = mongoClient.db.OPD_Data.insert_one(opdData)
     except Exception as e:
+
+        # Catching exception which will be raised during insert operation
+
         return jsonify(
             message="Error Occured",
             error=e,
-            ),400
+        ), 400
+
+    # If data got stored properly in database, Returning the id of the document to the insterface for future reference
 
     return jsonify(
-        message="Data Added Successfully"
-        ),200
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        documentId=str(dataId.inserted_id)
+    ), 200
 
     # # place= 'Attavar'
     # # res =  requests.get("https://api.mapbox.com/geocoding/v5/mapbox.places/"+place+".json?limit=2&access_token=pk.eyJ1IjoiYWxsZW5wZXRlciIsImEiOiJjbDJqN2V4eWYwdTR4M2pwOThpaXkxdnZ0In0.pmQrDt3zVDstenMMZF5xvg")
--- src/screens/form/FormScreen.jsx
+++ src/screens/form/FormScreen.jsx
@@ -2,95 +2,130 @@ import { Col, Row } from 'react-bootstrap'
 import styles from './FormScreen.module.css'
 import { useDropzone } from 'react-dropzone'
 import Close from '../../assets/images/Close.png'
-import { useState, useEffect } from 'react';
-import Header from '../../components/Header';
+import { useState, useEffect } from 'react'
+import Header from '../../components/Header'
 
-function FormScreen(params) {
-    var [address, setAddress] = useState([]);
+function FormScreen (params) {
+  var [address, setAddress] = useState([])
 
-    var [addressField, setAddressField] = useState("");
+  var [addressField, setAddressField] = useState('')
 
-    const [mapAddress, setMapAddress] = useState([]);
+  const [mapAddress, setMapAddress] = useState([])
 
-    const addAddress = (add) => {
-        var isStart = false;
-        if (address.length === 0) {
-            isStart = true;
-        }
-
-        var data = {
-            "address": add,
-            "isStart": isStart
-        }
-
-        var newAddress = [...address]
-
-        newAddress.push(data)
-
-        setAddress(newAddress);
-
-        setAddressField("")
+  const addAddress = add => {
+    var isStart = false
+    if (address.length === 0) {
+      isStart = true
     }
 
-    const removeAddress = (index) => {
-        var newAddress = [...address]
-
-        newAddress.splice(index, 1)
-
-        setAddress(newAddress)
+    var data = {
+      address: add,
+      isStart: isStart
     }
 
-    useEffect(() => {
+    var newAddress = [...address]
 
+    newAddress.push(data)
+
+    setAddress(newAddress)
+
+    setAddressField('')
+  }
+
+  const removeAddress = index => {
+    var newAddress = [...address]
+
+    newAddress.splice(index, 1)
+
+    setAddress(newAddress)
+  }
+
+  useEffect(() => {})
+
+  const submitForm = async () => {
+    if (address.length < 2) {
+      alert('Enter atleast 2 locations before submitting')
+    }
+
+    var response = await fetch('http://127.0.0.1:5000/upload_form', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify({ address: address })
     })
 
-    return (
-        <div >
-            <Header title="Fill Form"/>
-            <Row className={styles.container}>
-                <Col md={6} className={styles.leftContainer}>
-                    <div className={styles.inputField}>
-                        <input alt='Form' placeholder='Enter Address' required={true} value={addressField} onChange={(value) => { setAddressField(value.target.value) }} />
-                        <div className={styles.addButton} onClick={(e) => {
-                            addAddress(addressField)
-                        }}>
-                            <h5 className={styles.plusText}>+</h5>
-                        </div>
-                    </div>
-                </Col>
-                {address.length !== 0 ? (
-                    < Col md={6} className={styles.rightContainerCol}>
-                        {address.map((value, index) => (
-                            <div className={styles.itemContainer} key={index}>
-                                <p className={styles.address}>{value.address}</p>
-                                <div>
-                                    <img
-                                        src={Close}
-                                        alt='close'
-                                        height={15}
-                                        className={styles.closeIcon}
-                                        onClick={() => {
-                                            removeAddress(index)
-                                        }}
-                                    />
-                                </div>
+    var responseData = await response.json()
 
-                            </div>
-                        )
-                        )}
-                        <div className={styles.submitButton}>
-                            <span>Submit</span>
-                        </div>
-                    </Col>
-                ) : <Col md={6} className={styles.noFileContainer}>
-                    <p>Please add address to see here</p>
-                </Col>
+    if (response.status === 200) {
+      console.log(responseData.documentId)
+      alert('Done')
+    } else {
+      alert('Some error occured')
+    }
+  }
 
-                }
-
-            </Row >
-        </div>
-    )
+  return (
+    <div>
+      <Header title='Fill Form' />
+      <Row className={styles.container}>
+        <Col md={6} className={styles.leftContainer}>
+          <div className={styles.inputField}>
+            <input
+              alt='Form'
+              placeholder='Enter Address'
+              required={true}
+              value={addressField}
+              onChange={value => {
+                setAddressField(value.target.value)
+              }}
+            />
+            <div
+              className={styles.addButton}
+              onClick={e => {
+                addAddress(addressField)
+              }}
+            >
+              <h5 className={styles.plusText}>+</h5>
+            </div>
+          </div>
+        </Col>
+        {address.length !== 0 ? (
+          <Col md={6} className={styles.rightContainerCol}>
+            {address.map((value, index) => (
+              <div className={styles.itemContainer} key={index}>
+                <p className={styles.address}>{value.address}</p>
+                <div>
+                  <img
+                    src={Close}
+                    alt='close'
+                    height={15}
+                    className={styles.closeIcon}
+                    onClick={() => {
+                      removeAddress(index)
+                    }}
+                  />
+                </div>
+              </div>
+            ))}
+            <div
+              className={styles.submitButton}
+              onClick={() => {
+                submitForm()
+              }}
+            >
+              <span>Submit</span>
+            </div>
+          </Col>
+        ) : (
+          <Col md={6} className={styles.noFileContainer}>
+            <p>Please add address to see here</p>
+          </Col>
+        )}
+      </Row>
+    </div>
+  )
 }
 
-export default FormScreen;
+export default FormScreen
